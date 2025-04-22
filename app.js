c6ef0ca41c3f19db0d84a9824b0b4942a12e308b
nathalia <nathaliafloresmartins4550@gmail.com>
2025-04-22 09:39:26 -0300

    :books: docs: criacao da documentacao da API com swagger

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 import express from "express";
 
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
 //IMPORTA AS ROTAS
 import profilaxiaRaivaRoute from "./routes/profilaxiaRaiva.routes.js";
 import zoonosesRoute from "./routes/zoonoses.routes.js";
 import prevencaoLeptoRoute from "./routes/prevencaoLepto.routes.js";
 import tratamentoChikungunyaRoute from "./routes/tratamentoChikungunya.routes.js";
 
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 
 const app = express();
 //MIDDLEWARES
 app.use(express.json()); //PERMITE LER JSON NO CORPO DA REQ
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //ROTAS
 app.use("/profilaxiaraiva", profilaxiaRaivaRoute);
 app.use("/zoonoses", zoonosesRoute);
