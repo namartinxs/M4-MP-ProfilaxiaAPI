55e52136f600f8f9ac3a26a1da963349021efce1
nathalia <nathaliafloresmartins4550@gmail.com>
2025-07-09 15:31:14 -0300

    changes for better ui

--- models/utils/tratamentoChikungunya/tratamento.js
+++ models/utils/tratamentoChikungunya/tratamento.js
@@ -1,26 +1,25 @@
 const tratamento = {
   importante: [
-    "Importante: não existe tratamento antiviral específico para a Chikungunya. O cuidado é sintomático.",
+    "Não existe tratamento antiviral específico para a Chikungunya. O cuidado é sintomático.",
   ],
   fase_aguda: [
-    "#Fase aguda (até 10 dias)",
     "* Repouso",
     "* Hidratação (beber bastante líquido)",
     " * Medicamentos para dor e febre (paracetamol ou dipirona)",
-
-    "->Evitar anti-inflamatórios como ibuprofeno e AAS (podem agravar complicações hemorrágicas)",
+  ],
+  atencao_aguda: [
+    "Evitar anti-inflamatórios como ibuprofeno e AAS (podem agravar complicações hemorrágicas)",
   ],
   fase_subaguda: [
-    "#Fase subaguda e crônica (após 10 dias até meses)",
     "* Analgésicos e anti-inflamatórios prescritos por médico (como diclofenaco, nimesulida, etc.)",
     "* Fisioterapia para aliviar dor articular",
     "* Em casos persistentes e graves, o médico pode indicar corticóides por curto prazo",
-
-    "-> Observação:",
+  ],
+  atencao_subaguda: [
     "Crianças, idosos, gestantes e pessoas com comorbidades devem ser acompanhadas de perto.",
 
     "Não se automedique. Procure atendimento médico.",
   ],
 };
 
-export default tratamento
+export default tratamento;
